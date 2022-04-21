0753e718bbc793e99b785d8bddf8eb9f234ba45b
kemu <kemu430113@gmail.com>
2022-04-21 19:44:47 +0900

    add type hint

--- src/apps/views.py
+++ src/apps/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.db.models import QuerySet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -7,5 +8,5 @@ from .serializers import UserSerailizer
 
 class UserViewSets(ModelViewSet):
     serializer_class = UserSerailizer
-    queryset = User.objects.all()
+    queryset: QuerySet[User] = User.objects.all()
     permission_classes = (IsAuthenticated,)
